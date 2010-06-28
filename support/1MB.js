61ba753a304df0756d05360212d36ecc7d066006
Ben Burkert <ben@benburkert.com>
2010-06-27 18:47:04 -0700

    Add node benchmarks.

---
+++ support/1MB.js
@@ -0,0 +1,13 @@
+var Buffer = require('buffer').Buffer,
+    http   = require('http');
+
+var buffer = new Buffer(1024 * 1024);
+
+for (i = 0; i < buffer.length; i++) {
+  buffer[i] = 0;
+}
+
+http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Length' : buffer.length});
+  res.end(buffer);
+}).listen(8081, '127.0.0.1');
