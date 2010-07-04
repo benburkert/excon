4c9db8e91e15c98c627a31808cdca642814c8203
Ben Burkert <ben@benburkert.com>
2010-07-04 12:04:25 -0700

    Don't modify the load path in the library.

--- lib/excon.rb
+++ lib/excon.rb
@@ -1,9 +1,3 @@
-__DIR__ = File.dirname(__FILE__)
-
-$LOAD_PATH.unshift __DIR__ unless
-  $LOAD_PATH.include?(__DIR__) ||
-  $LOAD_PATH.include?(File.expand_path(__DIR__))
-
 require 'cgi'
 require 'openssl'
 require 'socket'
